88b7f290327c3df1cc3d0f6cc5736c7f8bae4401
inkuksuss <inkuksuss@naver.com>
2026-05-15 14:41:18 +0900

    init

---
+++ src/main/java/com/example/practice_querydsl/PracticeQuerydslApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice_querydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeQuerydslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeQuerydslApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practice_querydsl/PracticeQuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeQuerydslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
